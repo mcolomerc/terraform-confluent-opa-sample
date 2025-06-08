1d06e2002e5bc8177bc70ad2248b35821211ac7b
Marcelo Colomer <mcolomercornejo@confluent.io>
2025-06-08 14:41:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ module "topics" {
     source = "github.com/mcolomerc/terraform-confluent-topics" 
     environment = var.environment
     cluster = var.cluster
-    rbac_enabled = var.rbac_enabled
-    serv_account = var.serv_account
+    rbac_enabled = var.rbac_enabled 
     topics = var.topics
     cluster_credentials = {
         api_key=var.kafka_api_key
